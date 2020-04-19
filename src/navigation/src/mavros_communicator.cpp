33e2057e091ad8aed411f1f36b670620675b4356
rishi <rishi.veerepalli@gmail.com>
2020-04-19 15:46:59 -0700

    add a description to mavros communicator

--- src/navigation/src/mavros_communicator.cpp
+++ src/navigation/src/mavros_communicator.cpp
@@ -1,6 +1,10 @@
 #include "mavros_communicator.h"
 #include "main.h"
 
+/*
+  "Low-level" communication functions (publishing rc commands, getting IMU data, etc).
+ */
+
 void publisher(int channel, int pwm) {
   mavros_msgs::OverrideRCIn msg;
   msg.channels[channel-1] = pwm;
